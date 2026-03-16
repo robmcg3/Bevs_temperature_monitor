f739370fba8e72b4b091429d963f31f2a9ea99f9
robmcg3 <robmcg3@hotmail.com>
2026-03-16 08:53:30 -0400

    Update main.py
    Pasted in draft code

--- Temp1602/main.py
+++ Temp1602/main.py
@@ -1,1 +1,84 @@
-#placeholder
+# updated for smaller display 1602 on pico zero
+
+"""
+
+uses a dht22 and 1602 lcd display to show the temperature and humidity. 
+oled code heavily based on code posted by A Costas, referenced below
+
+┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+┃ Raspberry Pi Pico SSD1306 OLED Display (MicroPython)     ┃
+┃  https://wokwi.com/projects/359558101922696193$0         ┃
+┃                                                          ┃
+┃ Copyright (c) 2023 Anderson Costa                        ┃
+┃ GitHub: github.com/arcostasi                             ┃
+┃ License: MIT                                             ┃
+┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+"""
+
+
+from machine import Pin, I2C
+from ssd1306 import SSD1306_I2C
+import sys
+import utime
+import dht
+
+pix_res_x = 128
+pix_res_y = 32 #resolution has been updated
+
+
+
+sensor = dht.DHT22(Pin(0)) #pin ok
+
+
+def init_i2c(scl_pin, sda_pin):
+    # Initialize I2C device
+    i2c_dev = I2C(1, scl=Pin(scl_pin), sda=Pin(sda_pin), freq=200000)
+    i2c_addr = [hex(ii) for ii in i2c_dev.scan()]
+    
+    if not i2c_addr:
+        print('No I2C Display Found')
+        sys.exit()
+    else:
+        print("I2C Address      : {}".format(i2c_addr[0]))
+        print("I2C Configuration: {}".format(i2c_dev))
+    
+    return i2c_dev
+"""
+commented out for a smaller screen
+def display_text(oled): #update coordinates or remove
+    # Display text on the OLED
+    oled.text("Bev's Personal", 5, 5)
+    oled.text("Humidity Sensor", 5, 15)
+    oled.show()
+"""
+def display_anima(oled):
+
+    while True:
+        
+        # location definition  text def text(self, string, x, y, col=1)
+        
+        # Clear the specific line by drawing a filled black rectangle
+        #oled.fill_rect(45, 35, 60, 35, 1)#update coordinates
+        oled.fill(0) # change to clear entire screen
+        sensor.measure()
+        temp = sensor.temperature()
+        hum = sensor.humidity()
+
+        # oled.text(str(hum) + " %" , 45, 50)
+        # oled.text(str(temp) + " C", 45, 35)
+        
+        oled.text( "{:.1f}".format(temp)+ " C  ", 45, 5)#updated coordinates
+        oled.text( "{:.1f}".format(hum)+ " %  ", 45, 15)#updated coordinates
+
+        oled.show()
+        utime.sleep_ms(5000)#updated from one second to 5 seconds
+
+def main():
+    i2c_dev = init_i2c(scl_pin=27, sda_pin=26) #pins okay
+    oled = SSD1306_I2C(pix_res_x, pix_res_y, i2c_dev)
+    display_text(oled)
+    display_anima(oled)
+
+if __name__ == '__main__':
+    main
+    
